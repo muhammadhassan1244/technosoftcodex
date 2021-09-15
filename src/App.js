e758d60681d2969df0b7451940645bd12e03bd7c
DESKTOP-GLO9BBH\THE BOSS LAPTOP <2016cs438@student.uet.edu.pk>
2021-09-15 13:15:52 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ import FreshGradHiring from './components/Careers/FreshGradHiring';
 function App() {
   return (
     <div >
-        <BrowserRouter basename="/technosoftcodex/">
-   <Route exact path='/' component={Landing}/>
+        <BrowserRouter basename="/technosoftcodex">
+   <Route exact path='/technosoftcodex' component={Landing}/>
    <Route exact path='/whytech' component={WhyTechnosoft}/>
    <Route exact path='/web-app-development' component={WebAppDevelopment}/>
    <Route exact path='/mobile-app-development' component={MobileAppDevelopment}/>
